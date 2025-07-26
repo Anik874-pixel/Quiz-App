11050ce0afb738f1bc5ace0a3ff57a449c2055c0
Anik874-pixel <aniketkumar267@gmail.com>
2025-07-27 04:04:17 +0530

    Fixed CORS origins

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://quiz-app-one-omega-83.vercel.app/"], 
+    allow_origins=["https://quiz-app-one-omega-83.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
